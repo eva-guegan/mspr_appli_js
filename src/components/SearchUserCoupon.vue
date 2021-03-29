9c770c81a0e9cb5105c2433594e824b041a9c94a
eva.guegan <eva.guegan@hotmail.fr>
2021-03-29 09:02:55 +0200

    début barre de recherche

--- src/components/SearchUserCoupon.vue
+++ src/components/SearchUserCoupon.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="hello">
-    <p>Barre de recherche</p>
+<!--    <b-input-group prepend="Série" class="mt-3">-->
+<!--      <b-form-input placeholder="Recherche par titre" v-model="showTitle"></b-form-input>-->
+<!--      <b-input-group-append>-->
+<!--        <b-button variant="primary" @click="findCouponByUsed()">Confirm</b-button>-->
+<!--      </b-input-group-append>-->
+<!--    </b-input-group>-->
+<!--    <br /> <br />-->
+    <b-form-checkbox v-model="checked1" name="check-button" button v-on:click="findCouponByUsed()">
+      Afficher les coupons non utiliser <b>(Checked: {{ checked1 }})</b>
+    </b-form-checkbox>
+    <br />
+    <b-form-checkbox id="checkbox-1" v-model="status" name="checkbox-1" value="accepted" unchecked-value="not_accepted" @click="findCouponByUsed()">
+      Afficher les coupons non utiliser
+    </b-form-checkbox>
+
+    <div>State: <strong>{{ status }}</strong></div>
 
   </div>
 </template>
@@ -8,8 +23,27 @@
 <script>
 export default {
   name: 'SearchUserCoupon',
-  props: {
-    msg: String
+  data() {
+    return {
+      results: {},
+      showTitle: "",
+      status: 'not_accepted',
+      checked1: false,
+    };
+  },
+  methods: {
+    findCouponByUsed() {
+      console.log(this.status);
+      // this.axios
+      //     .get(this.$root.baseApi + "search")
+      //     .then((response) => {
+      //       this.results = response.data;
+      //
+      //       if (this.results.length > 0) {
+      //         this.$emit("searchCouponByUsed", this.results)
+      //       }
+      //     })
+    }
   }
 }
 </script>
